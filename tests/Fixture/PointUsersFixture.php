ecd96736bd9ea4f9aff70dbe96861eaf178a6984
hideichi saito <binbin4649@gmail.com>
2026-08-20 19:27:17 +0900

    PointUsersFixture.php fix.

--- tests/Fixture/PointUsersFixture.php
+++ tests/Fixture/PointUsersFixture.php
@@ -8,7 +8,8 @@ use Cake\TestSuite\Fixture\TestFixture;
 /**
  * PointUsersFixture
  *
- * 決済成功時に PointService::charge() がポイントを加算する対象。user 1〜3 に prepaid 残高を用意する。
+ * 決済成功時に PointService::charge() がポイントを加算する対象。
+ * 1 company = 1 PointUser。user 1 は company 1、user 3（suspended 課金）は company 2。
  */
 class PointUsersFixture extends TestFixture
 {
@@ -27,30 +28,10 @@ class PointUsersFixture extends TestFixture
             ],
             [
                 'id' => 2,
-                'user_id' => 2,
-                'company_id' => 1,
-                'type' => 'prepaid',
-                'point' => 1000,
-                'credit' => 0,
-                'created' => '2026-05-23 18:23:57',
-                'modified' => '2026-05-23 18:23:57',
-            ],
-            [
-                'id' => 3,
                 'user_id' => 3,
-                'company_id' => 1,
+                'company_id' => 2,
                 'type' => 'prepaid',
                 'point' => 500,
-                'credit' => 0,
-                'created' => '2026-05-23 18:23:57',
-                'modified' => '2026-05-23 18:23:57',
-            ],
-            [
-                'id' => 4,
-                'user_id' => 5,
-                'company_id' => 1,
-                'type' => 'prepaid',
-                'point' => 0,
                 'credit' => 0,
                 'created' => '2026-05-23 18:23:57',
                 'modified' => '2026-05-23 18:23:57',
